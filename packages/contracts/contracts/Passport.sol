06528f87d031f34c0f819b5b6bc45629ff53f06c
saitejareddysiddareddy <saitejareddysiddareddy@gmail.com>
2022-02-04 19:59:11 +0530

    Review comments

--- packages/contracts/contracts/Passport.sol
+++ packages/contracts/contracts/Passport.sol
@@ -4,12 +4,17 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import './ERC2981ContractWideRoyalties.sol';
-
 /**
  * The Passport contract does this and that...
  */
-contract Passport is ERC721, Ownable, ERC2981ContractWideRoyalties {
+contract Passport is ERC721, Ownable {
+  bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+
+  struct RoyaltyInfo {
+      address recipient;
+      uint24 amount;
+  }
+
   address payable public _owner;
   address payable public _cabindao = payable(0x8dca852d10c3CfccB88584281eC1Ef2d335253Fd);
   mapping(uint256 => bool) public sold;
@@ -18,6 +23,7 @@ contract Passport is ERC721, Ownable, ERC2981ContractWideRoyalties {
   uint256[] public tokenIds;
   string public metadataHash;
   uint256 public royaltyPcnt;
+  RoyaltyInfo private _royalties;
   event Purchase(address owner, uint256 price, uint256 id, string uri);
   constructor(address owner_, string memory name_, string memory symbol_, uint256 _supply, uint256 _price, string memory _metadataHash, uint256 _royaltyPcnt) ERC721(name_, symbol_) {
     _owner = payable(owner_);
@@ -33,10 +39,33 @@ contract Passport is ERC721, Ownable, ERC2981ContractWideRoyalties {
       public
       view
       virtual
-      override(ERC721, ERC2981Base)
+      override
       returns (bool)
   {
-      return super.supportsInterface(interfaceId);
+      return (interfaceId == _INTERFACE_ID_ERC2981) || super.supportsInterface(interfaceId);
+  }
+
+  /// @notice Called with the sale price to determine how much royalty
+  //          is owed and to whom.
+  /// @param value - the sale price of the NFT asset specified by _tokenId
+  /// @return receiver - address of who should be sent the royalty payment
+  /// @return royaltyAmount - the royalty payment amount for value sale price
+  function royaltyInfo(uint256, uint256 value)
+      external
+      view
+      returns (address receiver, uint256 royaltyAmount)
+  {
+      RoyaltyInfo memory royalties = _royalties;
+      receiver = royalties.recipient;
+      royaltyAmount = (value * royalties.amount) / 10000;
+  }
+
+  /// @dev Sets token royalties
+  /// @param recipient recipient of the royalties
+  /// @param value percentage (using 2 decimals - 10000 = 100, 0 = 0)
+  function _setRoyalties(address recipient, uint256 value) internal {
+      require(value <= 10000, 'ERC2981Royalties: Too high');
+      _royalties = RoyaltyInfo(recipient, uint24(value));
   }
 
   /// @notice Allows to set the royalties on the contract
