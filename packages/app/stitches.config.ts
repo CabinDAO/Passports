f22464f4018de58f9632b5fd6524ae4ab007464f
David Vargas <dvargas92495@gmail.com>
2022-03-03 21:43:09 -0600

    combine topo's and app's css text

--- packages/app/stitches.config.ts
+++ packages/app/stitches.config.ts
@@ -1,10 +1,17 @@
 import { createStitches } from "@stitches/react";
+import { getCssText as topoGetCssText } from "@cabindao/topo/lib/stitches.config";
 import { theme, media, utils } from "@cabindao/topo-theme";
 
-export const { styled, css, getCssText, globalCss, keyframes } = createStitches(
-  {
-    theme,
-    media,
-    utils,
-  }
-);
+export const {
+  styled,
+  css,
+  getCssText: appGetCssText,
+  globalCss,
+  keyframes,
+} = createStitches({
+  theme,
+  media,
+  utils,
+});
+
+export const getCssText = () => appGetCssText().concat(topoGetCssText());
