c8c1eb68e5eb67393d4910da255b8569215d4320
David Vargas <dvargas92495@gmail.com>
2022-04-26 13:59:31 -0600

    IPFS Pinning through infura (#46)
    * IPFS Pinning through infura
    
    * logo cid uploads also pinned

--- packages/app/pages/api/admin/stamp.ts
+++ packages/app/pages/api/admin/stamp.ts
@@ -1,20 +1,19 @@
 import { initializeApp } from "firebase/app";
-import {
-  collection,
-  doc,
-  getDocs,
-  getFirestore,
-  query,
-  setDoc,
-  where,
-} from "firebase/firestore/lite";
+import { collection, doc, getFirestore, setDoc } from "firebase/firestore/lite";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { addPins } from "../../../components/backend";
 import { firebaseConfig } from "../../../components/constants";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
-    case "POST":
-      const { address, contract, chain, version } = req.body;
+    case "POST": {
+      const {
+        address,
+        contract,
+        chain,
+        version,
+        files = [],
+      } = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
       const app = initializeApp(firebaseConfig);
       const db = getFirestore(app);
       const adminStampsCol = collection(db, "admin_stamps");
@@ -34,14 +33,37 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
           chain,
         }),
       ])
+        .then(() => addPins(files))
+        .then(() => {
+          res.status(200).json({ success: true });
+        })
+        .catch((e) => {
+          res
+            .status(e.response?.status || 500)
+            .json({ message: e.response?.data || e.message });
+        });
+    }
+    case "PUT": {
+      const { address, contract, chain } =
+        typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+      const app = initializeApp(firebaseConfig);
+      const db = getFirestore(app);
+      const adminStampsCol = collection(db, "admin_stamps");
+      const contractDoc = doc(adminStampsCol);
+      return setDoc(contractDoc, {
+        address: address.toLowerCase(),
+        contract: contract.toLowerCase(),
+        chain,
+      })
         .then(() => {
           res.status(200).json({ success: true });
         })
         .catch((e) => {
           res.status(500).json({ message: e.message });
         });
+    }
     default:
-      res.setHeader("Allow", ["POST"]);
+      res.setHeader("Allow", ["POST", "PUT"]);
       res.status(405).end(`Method ${req.method} Not Allowed`);
       break;
   }
